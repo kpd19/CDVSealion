325ca47f078e9aba741cccd5d416061380fa809b
Christopher Moore <life.dispersing@gmail.com>
2016-03-23 12:56:23 -0400

    Add max and kill craplot

--- Tests/Load&AnalyzeRData.R
+++ Tests/Load&AnalyzeRData.R
@@ -34,10 +34,18 @@
 		((total.var(arr))^0.5)/total.mean(arr)
 		}
 
+	max.y1.pack <- function(arr){
+		max(arr[,1:365])
+	}
+
+	max.y1.island <- function(arr){
+		max(colSums(arr[,1:365]))
+	}
+	
 
 # 1. list of functions
-	function.list <- list(end.mean, end.var, end.cv, total.mean, total.var, total.cv)
-	function.list.names <- c("end.mean", "end.var", "end.cv", "total.mean", "total.var", "total.cv")
+	function.list <- list(end.mean, end.var, end.cv, total.mean, total.var, total.cv, max.y1.pack, max.y1.island)
+	function.list.names <- c("end.mean", "end.var", "end.cv", "total.mean", "total.var", "total.cv", "max.y1.pack", "max.y1.island")
 
 # 2. name variables
 	arr.list.names <- c("S_D", "S_SL", "E_D", "E_SL", "I_D", "I_SL", "R_D", "R_SL", "D_D", "D_SL", "TD_D", "TD_SL")
@@ -60,30 +68,4 @@
 		for (j in 1:length(function.list)){
 				stats.arr[, j, i] <- unlist(lapply(arr.list, function.list[[j]]))
 		}
-		# print(stats.arr[,,j])
 	}
-
-# 7. example plot
-	dim(stats.arr)
-	pack.size.seq.1 <- seq(from = 1, to = dim(stats.arr)[3], by = 5)
-	pack.size.seq.10 <- seq(from = 2, to = dim(stats.arr)[3], by = 5)
-	pack.size.seq.100 <- seq(from = 3, to = dim(stats.arr)[3], by = 5)
-	pack.size.seq.50 <- seq(from = 4, to = dim(stats.arr)[3], by = 5)
-	pack.size.seq.500 <- seq(from = 5, to = dim(stats.arr)[3], by = 5)
-
-	ones <- stats.arr[which(rownames(stats.arr)=="D_SL"), which(colnames(stats.arr)=="end.mean"), pack.size.seq.1]
-	tens <- stats.arr[which(rownames(stats.arr)=="D_SL"), which(colnames(stats.arr)=="end.mean"), pack.size.seq.10]
-	fifty <- stats.arr[which(rownames(stats.arr)=="D_SL"), which(colnames(stats.arr)=="end.mean"), pack.size.seq.50]
-	huns <- stats.arr[which(rownames(stats.arr)=="D_SL"), which(colnames(stats.arr)=="end.mean"), pack.size.seq.100]
-	fivehun <- stats.arr[which(rownames(stats.arr)=="D_SL"), which(colnames(stats.arr)=="end.mean"), pack.size.seq.500]
-
-	ymin <- min(stats.arr[which(rownames(stats.arr)=="D_SL"), which(colnames(stats.arr)=="end.mean"),])
-	ymax <- max(stats.arr[which(rownames(stats.arr)=="D_SL"), which(colnames(stats.arr)=="end.mean"),])
-
-plot.col <- c("red", "orange", "green", "blue", "purple")
-
-	plot(rep(1, 5), log(ones), xlim = c(0, 500), ylim = c(log(ymin), log(ymax)), col = plot.col[1], cex = 1.25)
-	points(rep(10, 5), log(tens), col = plot.col[2], cex = 1.25)
-	points(rep(50, 5), log(fifty), col = plot.col[3], cex = 1.25)
-	points(rep(100, 5), log(huns), col = plot.col[4], cex = 1.25)
-	points(rep(500, 5), log(fivehun), col = plot.col[5], cex = 1.25)
